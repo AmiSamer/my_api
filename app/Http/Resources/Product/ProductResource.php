675619b8039701c9be63e82597b3badedf2a3d1c
acquaintsam <acquaintsam@gmail.com>
2022-10-11 17:00:20 +0600

    showing data

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -19,7 +19,13 @@ class ProductResource extends JsonResource
         return [
 
             'name' => $this->name,
-            'product_details' => $this->detail
+            'product_details' => $this->detail,
+            'product_price' => $this->price,
+            'stock' => $this->stock == 0 ? 'nothing is in stock' : $this->stock,
+            'discount_on_product' => $this->discount,
+            'total_price' =>  round((1-($this->discount/100))*$this->discount,2),
+            'product_ratings' =>  $this->reviews->count() > 0? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'no ratings',
+            'href' => route('reviews.index',$this->id)
 
         ];
     }
