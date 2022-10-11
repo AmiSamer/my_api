72dad5d747f4b15c54995f16751e6d91e9a1d791
acquaintsam <acquaintsam@gmail.com>
2022-10-11 13:46:33 +0600

    fill database using faker via seeder

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -4,9 +4,16 @@
 
 use Faker\Generator as Faker;
 use App\Model\Review;
+use App\Model\Product;
 
 $factory->define(Review::class, function (Faker $faker) {
     return [
-        //
+        
+        'product_id' => function(){
+        	return Product::all()->random();
+        },
+        'customer' => $faker->word,
+        'review' => $faker->paragraph,
+        'star' => $faker->numberBetween(0,5)
     ];
 });
