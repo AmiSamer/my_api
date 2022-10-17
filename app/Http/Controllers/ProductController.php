18373911535d4bc30589c8709644a7f690e52a2e
acquaintsam <acquaintsam@gmail.com>
2022-10-17 12:03:05 +0600

    product delete

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -120,7 +120,7 @@ class ProductController extends Controller
 
         $product -> update($request->all());
 
-        
+
          return response([
             'data' => new ProductResource($product)
         ],Response::HTTP_CREATED);
@@ -134,6 +134,7 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product -> delete();
+        return response(null,Response::HTTP_NO_CONTENT);
     }
 }
